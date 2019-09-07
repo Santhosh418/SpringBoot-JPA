ddd97036752049fb334e0d1ad1083bc45d31b84b
Santhosh418 <santhosh.keshannagari@gmail.com>
2019-09-07 16:57:55 +0530

    Updated Controller

--- src/main/java/com/cts/springhibernate/employees/EmployeeController.java
+++ src/main/java/com/cts/springhibernate/employees/EmployeeController.java
@@ -2,38 +2,49 @@ package com.cts.springhibernate.employees;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-	
-	
 
 	@Autowired
 	EmployeeService employeeService;
-	
+
 	@GetMapping("/test")
 	public String testService() {
 		return "Success";
-		
+
 	}
-	
-	
-	
+
 	@PostMapping("/insert_employee_details")
 	public String saveEmployeeDetails(@RequestBody EmployeeDTO employee) {
 		String status = null;
-		System.out.println("State =  = = "+employee.getEmployeeDetailsDTO().getState());
+		System.out.println("State =  = = " + employee.getEmployeeDetailsDTO().getState());
 		try {
-		employeeService.insertEmployee(employee);
-		}catch(Exception ex){
-			status= "Failed to insert employee details";
+			employeeService.insertEmployee(employee);
+		} catch (Exception ex) {
+			status = "Failed to insert employee details";
 		}
-		status="Successfully inserted details";
+		status = "Successfully inserted details";
 		return status;
-		
+
 	}
+
+	@GetMapping("/employee/{id}")
+	public EmployeeDTO fetchEmployeeDetails(@PathVariable(value = "id") Long id) {
+		EmployeeDTO employee = null;
+		System.out.println("fetchEmployeeDetails ");
+		employee = employeeService.getEmployeeDetails(id);
+		if (employee == null) {
+			throw new EmployeeNotFoundException(id);
+		}
+		System.out.println("employee =" + employee);
+
+		return employee;
+
+	}
+
 }
